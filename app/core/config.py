35d7384b7521aad828735d509555b4e5f9a0a6f3
Guilherme-David <guidavido07@gmail.com>
2026-08-17 20:56:32 -0300

    Implementação do Alembic em Migrations

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = "sqlite:///db_Gamelog.db"
-SECRET_KEY = os.getenv("SECRET_KEY")
-RAWG_API_KEY = os.getenv("RAWG_API_KEY")
+class Settings:
+    DATABASE_URL = "mysql+pymysql://root:admin@localhost:3306/db_Gamelog"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    RAWG_API_KEY = os.getenv("RAWG_API_KEY")
+
+settings = Settings()
